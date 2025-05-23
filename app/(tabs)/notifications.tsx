25c29f1142bf06ecf3d5b04243ea0fa72a05ddc9
armand benjamin <armandbenjamin30@gmail.com>
2025-05-23 07:33:20 +0200

    refactor: update tab layout and remove unused components; add new screens for bookmarks, create, notifications, and profile

---
+++ app/(tabs)/notifications.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function notifications() {
+  return (
+    <View>
+      <Text>notifications</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function profile() {
+  return (
+    <View>
+        <Text>Profile</Text>
+    </View>
+  )
+}
